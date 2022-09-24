6b13710127975618867d29c2fe907027727379c4
Juan Paggi <paggi97@hotmail.com>
2022-09-24 11:47:45 -0300

    Basic backend structure

---
+++ backend/src/main/java/com/proyect/UserPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPlatformApplication.class, args);
+	}
+
+}
